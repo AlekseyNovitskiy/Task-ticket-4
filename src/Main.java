44f8becf6d35f678b04bb20b01e489ba598e0c11
AlekseyNovitskiy <novickii_aleksei@mail.ru>
2022-10-14 15:17:33 +0300

    Задача по курсовой

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         String[] listName = {"Alex", "Alex", "Grom", "Nikola", "Garry", "Nikola"};
-        String[] listName1 = new String[listName.length];
         int t = 0;
-        for (int i = 0; i < listName.length; i++) {
-            boolean duplicatFound = false;
-            for (int j = i+1; j < listName.length ; j++) {
-                if (listName[i].equals(listName[j])) {
-                    duplicatFound = true;
+        for (int i = listName.length -1; i > 0; i--) {
+            for (int j = i-1; j > -1; j--) {
+                if(listName[i].equals(listName[j])) {
+                    listName[i] = null;
                     break;
                 }
             }
-            if (!duplicatFound) {
-                listName1[t] = listName[i];
-                t++;
-            }
         }
-        String newString = "";
-        for (int i = 0; i < listName1.length; i++) {
-            if (listName1[i] != null) {
-                newString = newString + " " + listName1[i];
-            } else {
-                break;
-            }
-
+        StringBuilder allName = new StringBuilder();
+        for (int i = 0; i < listName.length; i++) {
+            if (listName[i] != null)
+                allName.append(listName[i] + " ");
         }
-        System.out.println(newString);
-
+        System.out.println(allName);
     }
 }
